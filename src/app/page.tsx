cd1a268aab7828aa343c8c9e24f5fd0198be6c19
pta201 <pta20801@gmail.com>
2023-07-29 21:29:09 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Signature from "@/component/Signature";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
+      <Signature />
+    </main>
+  );
+}
