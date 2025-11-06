2a46a00d20ef57c5dd746ed1da0ad808a751835c
pta208 <pta20801@gmail.com>
2025-11-06 11:45:08 +0700

    add vercel speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import AppLayout from "@/layout";
 import AppProvider from "@/providers";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <Analytics />
+      <SpeedInsights />
       <body className={inter.className}>
         <AppProvider>
           <AppLayout>{children}</AppLayout>
